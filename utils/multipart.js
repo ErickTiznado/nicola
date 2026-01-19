ab7c9ad2ded56f8ccc801c553ce1290f0153103c
ErickTiznado <88002820+ErickTiznado@users.noreply.github.com>
2026-01-19 10:14:03 -0600

    feat: Correcion de bugs en mulipart

--- utils/multipart.js
+++ utils/multipart.js
@@ -1,8 +1,9 @@
-import PatternBuilder from "../pattern-builder/PatternBuilder";
+import PatternBuilder from "../pattern-builder/PatternBuilder.js";
 const pattern = new PatternBuilder()
   .find("boundary=")
-  .group(null, {capture: true})
+  .group(null, { capture: true })
   .not('; ').oneOrMore()
+  .endGroup()
   .toRegex();
 
 const NAME_PATTERN = new PatternBuilder()
@@ -23,13 +24,13 @@ export const getBoundary = (data) => {
   return match ? match[1] : null;
 };
 
-function splitBuffer(buffer, separator){
-  const parts =  [];
+function splitBuffer(buffer, separator) {
+  const parts = [];
   let start = 0;
 
   let index = buffer.indexOf(separator, start);
 
-  while( index !== -1){
+  while (index !== -1) {
     let cut = buffer.subarray(start, index);
     parts.push(cut);
     start = index + separator.length;
@@ -48,35 +49,35 @@ export const parseMultipart = (bodyBuffer, boundary) => {
   const separator = Buffer.from(`--${boundary}`);
   let parts = splitBuffer(bodyBuffer, separator);
 
-  for(const p  of parts){
-    if(p.length < 4) continue;
+  for (const p of parts) {
+    if (p.length < 4) continue;
     const headerIndex = p.indexOf(Buffer.from('\r\n\r\n'));
-    if(headerIndex === -1) continue;
+    if (headerIndex === -1) continue;
 
     const headerPart = p.subarray(0, headerIndex).toString('utf-8');
     let bodyPart = p.subarray(headerIndex + 4, p.length)
 
-    if(bodyPart.length >= 2 && bodyPart[bodyPart.length -2] === 13 && bodyPart[bodyPart.length -1] ===10){
+    if (bodyPart.length >= 2 && bodyPart[bodyPart.length - 2] === 13 && bodyPart[bodyPart.length - 1] === 10) {
       bodyPart = bodyPart.subarray(0, bodyPart.length - 2)
-  }
+    }
 
-  const nameMatch = headerPart.match(NAME_PATTERN);
-  const filenameMatch = headerPart.match(FILENAME_PATTERN);
-  if(filenameMatch){
-    const filename = filenameMatch[1];
-    const name = nameMatch ? nameMatch[1] : 'file';
+    const nameMatch = headerPart.match(NAME_PATTERN);
+    const filenameMatch = headerPart.match(FILENAME_PATTERN);
+    if (filenameMatch) {
+      const filename = filenameMatch[1];
+      const name = nameMatch ? nameMatch[1] : 'file';
 
-    results.files[name] = {
-      filename,
-      type: 'application/octet-stream',
-      data: bodyPart,
-      size: bodyPart.length
+      results.files[name] = {
+        filename,
+        type: 'application/octet-stream',
+        data: bodyPart,
+        size: bodyPart.length
+      }
+    }
+    else if (nameMatch) {
+      const name = nameMatch[1];
+      results.fields[name] = bodyPart.toString('utf-8');
     }
   }
-  else if(nameMatch){
-    const name = nameMatch[1];
-    results.fields[name] = bodyPart.toString('utf-8');
-  }
-}
   return results;
 }
